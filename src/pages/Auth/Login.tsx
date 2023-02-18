d1290c3741fc96f30174ef505b5b0cc3e1529acb
fernando-yaeda <fernandoyudi.yaeda@gmail.com>
2023-02-17 23:11:41 -0300

    feat(login): created login page and login service

---
+++ src/pages/Auth/Login.tsx
@@ -0,0 +1,73 @@
+import { ChangeEvent, FormEvent, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
+import Button from "../../components/Header/Button";
+import Header from "../../components/Header/Header";
+import Logo from "../../components/Header/Logo";
+import {
+  Container,
+  Form,
+  HorizontalLine,
+  Input,
+  Link,
+  Title,
+} from "../../components/Auth/index";
+import { login } from "../../services/Login";
+
+export default function Login() {
+  const [loginInfo, setLoginInfo] = useState({
+    email: "",
+    password: "",
+  });
+
+  const navigate = useNavigate();
+
+  async function submit(event: FormEvent) {
+    event.preventDefault();
+    try {
+      await login(loginInfo.email, loginInfo.password);
+      toast.success("You have successfully logged in. Enjoy your experience!");
+      navigate("/login");
+    } catch (error) {
+      toast.error("Incorrect username or password. Please try again.");
+    }
+  }
+
+  return (
+    <>
+      <Header>
+        <Logo />
+      </Header>
+      <Container>
+        <Title>Login</Title>
+        <Form onSubmit={submit}>
+          <Input
+            placeholder="email"
+            type="email"
+            value={loginInfo.email}
+            onChange={(e) =>
+              setLoginInfo({ ...loginInfo, email: e.target.value })
+            }
+          />
+          <Input
+            placeholder="password"
+            type="password"
+            value={loginInfo.password}
+            onChange={(e) =>
+              setLoginInfo({ ...loginInfo, password: e.target.value })
+            }
+          />
+          <Button
+            type="submit"
+            width="100%"
+            onClick={() => console.log(loginInfo)}
+          >
+            Login
+          </Button>
+          <HorizontalLine />
+        </Form>
+        <Link to="/sign-up">Don't have an account? Sign up here!</Link>
+      </Container>
+    </>
+  );
+}
---
+++ src/services/Login.ts
@@ -0,0 +1,10 @@
+import api from "./api";
+
+export async function login(email: string, password: string) {
+  const response = await api.post("/auth/sign-in", {
+    email,
+    password,
+  });
+
+  return response.data;
+}
