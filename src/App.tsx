5b994dabdf2e2a64f6e9bae4d0e39a752316f0e5
fernando-yaeda <fernandoyudi.yaeda@gmail.com>
2023-02-17 23:08:36 -0300

    feat(app): implement react-toastify

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,23 @@
+import "react-toastify/dist/ReactToastify.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
 import Home from "./pages/Home";
-import SignUp from "./pages/SignUp";
+import SignUp from "./pages/Auth/SignUp";
+import Login from "./pages/Auth/Login";
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/sign-up" element={<SignUp />} />
-      </Routes>
-    </Router>
+    <>
+      <ToastContainer />
+
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/sign-up" element={<SignUp />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </Router>
+    </>
   );
 }
 
