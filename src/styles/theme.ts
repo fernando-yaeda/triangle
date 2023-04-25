926e36f0b6b2ddadaade077c462d31d3416240ef
fernando yaeda <fernandoyudi.yaeda@gmail.com>
2023-04-25 01:34:47 -0300

    refactor(styles): change to styled-components global styles and theme

---
+++ src/styles/theme.ts
@@ -0,0 +1,81 @@
+export const theme = {
+  black: "#000000",
+  white: "#FFFFFF",
+  purple: "#6F47EB",
+  grey: "#C5CC5C5",
+  lightGray: "#F9F9F9",
+  mediumGray: "#EDEDED",
+  darkGray: "#5C5F62",
+  lightBlue: "#EEE9FD",
+  lightRed: "#FBE4E1",
+  displayMd: {
+    fontSize: "26px",
+    fontWeight: 500,
+    lineHeight: "34px",
+  },
+  headingLg: {
+    fontSize: "24px",
+    fontWeight: 500,
+    lineHeight: "32px",
+  },
+  headingMd: {
+    fontSize: "20px",
+    fontWeight: 500,
+    lineHeight: "28px",
+  },
+  headingSm: {
+    fontSize: "18px",
+    fontWeight: 500,
+    lineHeight: "24px",
+  },
+  textXl: {
+    fontSize: "18px",
+    fontWeight: 500,
+    lineHeight: "26px",
+  },
+  textLg: {
+    fontSize: "16px",
+    fontWeight: 500,
+    lineHeight: "24px",
+  },
+  textMd: {
+    fontSize: "15px",
+    fontWeight: 500,
+    lineHeight: "23px",
+  },
+  textSm: {
+    fontSize: "14px",
+    fontWeight: 500,
+    lineHeight: "24px",
+  },
+  textXs: {
+    fontSize: "13px",
+    fontWeight: 500,
+    lineHeight: "21px",
+  },
+  textXlRegular: {
+    fontSize: "18px",
+    fontWeight: 400,
+    lineHeight: "26px",
+  },
+  textLgRegular: {
+    fontSize: "16px",
+    fontWeight: 400,
+    lineHeight: "24px",
+  },
+  textMdRegular: {
+    fontSize: "15px",
+    fontWeight: 400,
+    lineHeight: "23px",
+  },
+  textSmRegular: {
+    fontSize: "14px",
+    fontWeight: 400,
+    lineHeight: "24px",
+  },
+  textXsRegular: {
+    fontSize: "13px",
+    fontWeight: 400,
+    lineHeight: "21px",
+  },
+};
