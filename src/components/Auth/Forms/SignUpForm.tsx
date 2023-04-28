59f763c7e3ccd459e56e3de363e5419460e924de
fernando yaeda <fernandoyudi.yaeda@gmail.com>
2023-04-28 02:05:54 -0300

    feat(components): add validation to sugn up form

--- src/components/Auth/Forms/SignUpForm.tsx
+++ src/components/Auth/Forms/SignUpForm.tsx
@@ -1,11 +1,30 @@
+import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
+import { z } from "zod";
 import { Button } from "../../Button";
 import * as S from "./styles";
 
-export default function SignUpForm() {
-  const { register, handleSubmit } = useForm();
+const signUpFormSchema = z.object({
+  email: z
+    .string()
+    .nonempty("email field should not be empty")
+    .email("email field should have a valid email address")
+    .toLowerCase(),
+  password: z.string(),
+});
 
-  const handleSubmitData = (data: any) => {
+type SignUpFormData = z.infer<typeof signUpFormSchema>;
+
+export default function SignUpForm() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitting },
+  } = useForm<SignUpFormData>({
+    resolver: zodResolver(signUpFormSchema),
+  });
+
+  const handleSubmitData = async (data: SignUpFormData) => {
     console.log("submit", data);
   };
 
@@ -18,6 +37,7 @@ export default function SignUpForm() {
         placeholder="Email Address"
         type="email"
       />
+      {errors.email && <S.ErrorText>• {errors.email.message}</S.ErrorText>}
       <S.Label htmlFor="password">Password</S.Label>
       <S.Input
         {...register("password")}
@@ -25,9 +45,23 @@ export default function SignUpForm() {
         placeholder="Password"
         type="password"
       ></S.Input>
-      <Button type="submit" width="100%">
-        Login
-      </Button>
+      {errors.password && (
+        <S.ErrorText>• {errors.password.message}</S.ErrorText>
+      )}
+      {isSubmitting ? (
+        <Button
+          disabled={isSubmitting}
+          variant="whiteAndGrey"
+          type="submit"
+          width="100%"
+        >
+          Login
+        </Button>
+      ) : (
+        <Button type="submit" width="100%">
+          Login
+        </Button>
+      )}
     </S.Form>
   );
 }
