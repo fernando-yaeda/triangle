d0ee896ef3b76f4cd948f3107fa327414a8ae29d
fernando yaeda <fernandoyudi.yaeda@gmail.com>
2023-04-29 03:10:12 -0300

    feat(components): create navbar component

---
+++ src/components/Navbar/index.tsx
@@ -0,0 +1,82 @@
+import {
+  CalendarBlank,
+  Cards,
+  ChartLine,
+  Chat,
+  House,
+  SquaresFour,
+  Users,
+} from "@phosphor-icons/react";
+import { ReactNode } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
+import Logo from "../Logo";
+import * as S from "./styles";
+
+export default function Navbar() {
+  const location = useLocation();
+  return (
+    <S.Container>
+      <Logo />
+      <S.Navbar>
+        <NavItem to="/home" isActive={location.pathname === "/home"}>
+          <House size={24} weight="bold" />
+          Home
+        </NavItem>
+
+        <NavItem to="/dashboard" isActive={location.pathname === "/dashboard"}>
+          <ChartLine size={24} weight="bold" />
+          Dashboard
+        </NavItem>
+
+        <NavItem to="/teams" isActive={location.pathname === "/teams"}>
+          <Users size={24} weight="bold" />
+          Teams
+        </NavItem>
+
+        <NavItem to="/boards" isActive={location.pathname === "/boards"}>
+          <Cards size={24} weight="bold" />
+          Boards
+        </NavItem>
+
+        <NavItem to="/inbox" isActive={location.pathname === "/inbox"}>
+          <Chat size={24} weight="bold" />
+          Inbox
+        </NavItem>
+
+        <NavItem to="/timeline" isActive={location.pathname === "/timeline"}>
+          <CalendarBlank size={24} weight="bold" />
+          Timeline
+        </NavItem>
+
+        <NavItem
+          to="/more-options"
+          isActive={location.pathname === "/more-options"}
+        >
+          <SquaresFour size={24} weight="bold" />
+          More Options
+        </NavItem>
+      </S.Navbar>
+    </S.Container>
+  );
+}
+
+interface NavItemProps {
+  children: ReactNode;
+  to: string;
+  isActive: boolean;
+}
+
+function NavItem({ children, to, isActive }: NavItemProps) {
+  const navigate = useNavigate();
+
+  const handleClick = (event: React.MouseEvent<HTMLDivElement>) => {
+    event.preventDefault();
+    navigate(to);
+  };
+
+  return (
+    <S.NavItem onClick={handleClick} isActive={isActive}>
+      {children}
+    </S.NavItem>
+  );
+}
---
+++ src/components/Navbar/styles.ts
@@ -0,0 +1,59 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  height: 100%;
+  width: 288px;
+
+  background-color: ${(props) => props.theme.colors.lightGrey};
+
+  padding: 24px 24px;
+
+  border-right: 1px solid ${(props) => props.theme.colors.mediumGrey};
+
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: flex-start;
+`;
+
+export const Navbar = styled.nav`
+  height: 100%;
+  width: 100%;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: flex-start;
+  gap: 14px;
+
+  padding: 44px 0;
+`;
+
+export const NavItem = styled.div<{ isActive: boolean }>`
+  height: 44px;
+  width: 100%;
+
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  gap: 12px;
+
+  padding: 0 20px;
+  border-radius: 6px;
+
+  font-size: ${(props) => props.theme.fontVariants.textLg.fontSize};
+  line-height: ${(props) => props.theme.fontVariants.textLg.lineHeight};
+  font-weight: 500;
+
+  color: ${(props) =>
+    props.isActive ? props.theme.colors.black : props.theme.colors.darkGrey};
+  background-color: ${(props) =>
+    props.isActive ? props.theme.colors.mediumGrey : ""};
+
+  cursor: pointer;
+
+  &:hover {
+    color: ${(props) => props.theme.colors.black};
+    background-color: ${(props) => props.theme.colors.mediumGrey};
+  }
+`;
