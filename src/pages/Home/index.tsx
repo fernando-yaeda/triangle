c00d8aca3973b562691f0176f472c2d004ebc714
fernando yaeda <fernandoyudi.yaeda@gmail.com>
2023-04-30 18:44:55 -0300

    feat(pages): create main app home page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,5 +1,6 @@
 import styled from "styled-components";
-import Navbar from "../../components/Navbar";
+import Header from "../../components/Main/Header";
+import Navbar from "../../components/Main/Navbar";
 
 export default function Home() {
   // const { userData } = useContext(UserDataContext) as UserDataContextType;
@@ -7,15 +8,26 @@ export default function Home() {
   return (
     <Container>
       <Navbar />
+      <Header />
+      <ContentWrapper></ContentWrapper>
     </Container>
   );
 }
 
 const Container = styled.div`
-  height: 100vh;
+  min-height: 100vh;
   width: 100vw;
 
   display: flex;
 
   background-color: ${(props) => props.theme.colors.white};
+`;
+
+const ContentWrapper = styled.div`
+  height: 100%;
+  width: 100%;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
 `;
