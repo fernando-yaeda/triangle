eb168eeec351e741060d2dec0c4c4e72248e4a25
fernando yaeda <fernandoyudi.yaeda@gmail.com>
2023-05-01 05:17:54 -0300

    refactor(pages): apply changes due to componentizations

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,47 +1,11 @@
-import styled from "styled-components";
-import Header from "../../components/Main/Header";
-import Navbar from "../../components/Main/Navbar";
+import { PageWrapper } from "../../components/Main/PageWrapper";
 
 export default function Home() {
   // const { userData } = useContext(UserDataContext) as UserDataContextType;
 
   return (
-    <Container>
-      <Navbar />
-      <Main>
-        <Header />
-        <ContentWrapper></ContentWrapper>
-      </Main>
-    </Container>
+    <PageWrapper>
+      <h1>testing</h1>
+    </PageWrapper>
   );
 }
-
-const Container = styled.div`
-  height: 100vh;
-  width: 100%;
-
-  display: flex;
-
-  background-color: ${(props) => props.theme.colors.white};
-`;
-
-const Main = styled.div`
-  height: 100%;
-  width: 100%;
-
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-`;
-
-const ContentWrapper = styled.div`
-  height: 100%;
-  width: 100%;
-
-  display: flex;
-  flex-direction: column;
-  justify-content: flex-start;
-  align-items: center;
-
-  margin-top: 76px;
-`;
