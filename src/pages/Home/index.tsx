f794082d56e2edeeb1bce1ff7f7633f8a700ecbb
fernando yaeda <fernandoyudi.yaeda@gmail.com>
2023-05-03 22:16:29 -0300

    feat(pages): add base layout and cards

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,11 +1,44 @@
+import styled from "styled-components";
 import { PageWrapper } from "../../components/Main/PageWrapper";
+import { AnnouncementCard } from "./components/AnnouncementsCard";
+import { ProjectsCard } from "./components/ProjectsCard";
+import { SetUpAccountCard } from "./components/SetupAccountCard";
+import { TasksCard } from "./components/TasksCard";
 
 export default function Home() {
   // const { userData } = useContext(UserDataContext) as UserDataContextType;
 
   return (
     <PageWrapper>
-      <h1>testing</h1>
+      <Container>
+        <Row>
+          <SetUpAccountCard />
+        </Row>
+
+        <Row>
+          <TasksCard />
+          <AnnouncementCard />
+        </Row>
+
+        <Row>
+          <ProjectsCard />
+        </Row>
+      </Container>
     </PageWrapper>
   );
 }
+
+const Container = styled.div`
+  width: 100%;
+  flex: 0 0 auto;
+`;
+
+const Row = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-self: flex-start;
+  gap: 20px;
+
+  margin-bottom: 20px;
+`;
