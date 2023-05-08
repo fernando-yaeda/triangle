7d651165f4f7aa1a4d2d8b5040007a31295a3f3a
fernando yaeda <fernandoyudi.yaeda@gmail.com>
2023-05-08 16:05:52 -0300

    feat(hooks): add useLocalStorate hook

---
+++ src/hooks/useLocalStorage/useLocalStorage.tsx
@@ -0,0 +1,16 @@
+export function useLocalStorage() {
+  const setItem = (key: string, value: string) => {
+    localStorage.setItem(key, value);
+  };
+
+  const getItem = (key: string) => {
+    const value = localStorage.getItem(key);
+    return value;
+  };
+
+  const removeItem = (key: string) => {
+    localStorage.removeItem(key);
+  };
+
+  return { setItem, getItem, removeItem };
+}
