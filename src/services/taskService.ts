7b3b6bab133a7b35bac039c8e6cfd4e7113f8257
fernando yaeda <fernandoyudi.yaeda@gmail.com>
2023-05-11 23:37:37 -0300

    feat(services): create update status method

--- src/services/taskService.ts
+++ src/services/taskService.ts
@@ -7,6 +7,11 @@ export type CreateTaskParams = {
   dueDate: string | null;
 };
 
+export type PatchTaskParams = {
+  id: number;
+  status: "DONE" | "IN_PROGRESS" | "DONE";
+};
+
 async function create(
   data: CreateTaskParams,
   token: string | null
@@ -30,9 +35,27 @@ async function getTasks(token: string | null): Promise<Task[]> {
   return response.data;
 }
 
+async function updateStatus(
+  { id, status }: PatchTaskParams,
+  token: string | null
+): Promise<Task[]> {
+  const response = await api.patch(
+    `tasks/${id}/status`,
+    { status },
+    {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    }
+  );
+
+  return response.data;
+}
+
 const taskService = {
   create,
   getTasks,
+  updateStatus,
 };
 
 export default taskService;
