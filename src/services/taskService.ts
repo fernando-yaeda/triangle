cc5dedf9eeca5c94407e2f81043a31dad1af5ff2
fernando yaeda <fernandoyudi.yaeda@gmail.com>
2023-05-13 03:06:59 -0300

    refactor(services): implement new api filters

--- src/services/taskService.ts
+++ src/services/taskService.ts
@@ -12,10 +12,17 @@ export type PatchTaskParams = {
   status: "DONE" | "IN_PROGRESS" | "DONE";
 };
 
+type taskQuery = {
+  search?: string;
+  status?: "DONE" | "IN_PROGRESS" | "OPEN";
+  sortBy?: "dueDate" | "title" | "description";
+  orderBy?: "ASC" | "DESC";
+};
+
 async function create(
   data: CreateTaskParams,
   token: string | null
-): Promise<void> {
+): Promise<Task> {
   const response = await api.post("/tasks", data, {
     headers: {
       Authorization: `Bearer ${token}`,
@@ -25,8 +32,17 @@ async function create(
   return response.data;
 }
 
-async function getTasks(token: string | null): Promise<Task[]> {
-  const response = await api.get("/tasks", {
+async function getTasks(
+  token: string | null,
+  { search, status, sortBy, orderBy }: taskQuery
+): Promise<Task[]> {
+  let query = "/?";
+  if (search) query += `search=${search}&`;
+  if (status) query += `status=${status}&`;
+  if (sortBy) query += `sortBy=${sortBy}&`;
+  if (orderBy) query += `orderBy=${orderBy}&`;
+
+  const response = await api.get(`/tasks${query.length > 2 ? query : ""}`, {
     headers: {
       Authorization: `Bearer ${token}`,
     },
