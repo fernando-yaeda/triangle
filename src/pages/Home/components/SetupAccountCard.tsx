c99f2baa5aca8d7aa03ee3cee1d11535527d4ce0
fernando yaeda <fernandoyudi.yaeda@gmail.com>
2023-05-13 20:04:25 -0300

    feat(components): implement use auth

--- src/pages/Home/components/SetupAccountCard.tsx
+++ src/pages/Home/components/SetupAccountCard.tsx
@@ -1,14 +1,13 @@
 import { CaretRight } from "@phosphor-icons/react";
 import { Card } from "../../../components/Main/Card";
+import useAuth from "../../../hooks/useAuth/useAuth";
 
-interface SetUpAccountCardProps {
-  userSetUpAccount?: string;
-}
+export function SetUpAccountCard() {
+  const { user } = useAuth();
 
-export function SetUpAccountCard({ userSetUpAccount }: SetUpAccountCardProps) {
   return (
     <Card
-      title="Hi Johnson, you are almost done."
+      title={`Hi ${user?.firstName}, you are almost done.`}
       subtitle="Please complete few steps to setup your account completely."
       buttonText="Setup Account"
       buttonIcon={<CaretRight size={20} />}
