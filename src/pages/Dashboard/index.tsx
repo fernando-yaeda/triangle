e9296d2709636ae09312e3d9c68e8f6a2d902e72
fernando yaeda <fernandoyudi.yaeda@gmail.com>
2023-05-14 17:34:04 -0300

    feat(pages): add page wrapper to dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,23 +1,7 @@
-import styled from "styled-components";
-import Header from "../../components/Main/Header";
-import Navbar from "../../components/Main/Navbar";
+import { PageWrapper } from "../../components/Main/PageWrapper";
 
 export default function Dashboard() {
   // const { userData } = useContext(UserDataContext) as UserDataContextType;
 
-  return (
-    <Container>
-      <Navbar />
-      <Header />
-    </Container>
-  );
+  return <PageWrapper currentPage="Dashboard">test</PageWrapper>;
 }
-
-const Container = styled.div`
-  height: 100vh;
-  width: 100vw;
-
-  display: flex;
-
-  background-color: ${(props) => props.theme.colors.white};
-`;
