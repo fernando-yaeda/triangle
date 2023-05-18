036313e1aafcff731402c7def3ed54c14a1a6578
fernando yaeda <fernandoyudi.yaeda@gmail.com>
2023-05-18 02:35:40 -0300

    refactor(pages): implement use scroll position

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,4 +1,5 @@
 import { UserCircle } from "@phosphor-icons/react";
+import { useEffect, useRef, useState } from "react";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
 import AuthContainer from "../../components/Auth";
@@ -7,9 +8,29 @@ import Header from "../../components/Header/Header";
 import SignInForm from "./components/SignInForm";
 
 export default function SignIn() {
+  const [scrollY, setScrollY] = useState(0);
+  const containerRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (containerRef.current) {
+        setScrollY(containerRef.current.scrollTop);
+      }
+    };
+
+    if (containerRef.current) {
+      containerRef.current.addEventListener("scroll", handleScroll);
+    }
+
+    return () => {
+      if (containerRef.current) {
+        containerRef.current.removeEventListener("scroll", handleScroll);
+      }
+    };
+  }, []);
   return (
     <Container>
-      <Header />
+      <Header scrollY={scrollY} />
       <AuthContainer
         icon={<UserCircle size={110} />}
         title="Sign in to Triangle"
@@ -28,10 +49,12 @@ export default function SignIn() {
 }
 
 const Container = styled.div`
-  min-height: 100vh;
+  height: 100%;
   width: 100%;
 
   display: flex;
   flex-direction: column;
   align-items: center;
+
+  overflow: auto;
 `;
--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -4,34 +4,41 @@ import styled from "styled-components";
 import AuthContainer from "../../components/Auth";
 import Footer from "../../components/Footer/Footer";
 import Header from "../../components/Header/Header";
+import useScrollPosition from "../../hooks/useScrollPosition";
 import SignUpForm from "./components/SignUpForm";
 
 export default function SignUp() {
+  const { scrollY, containerRef } = useScrollPosition();
+
   return (
-    <Container>
-      <Header />
-      <AuthContainer
-        icon={<UserCircle size={110} />}
-        title="Sign up to Triangle"
-        subtitle={
-          <>
-            {"Already have an account? "}
-            <Link to={"/"}>Login</Link>
-          </>
-        }
-      >
-        <SignUpForm />
-      </AuthContainer>
-      <Footer />
-    </Container>
+    <>
+      <Container ref={containerRef}>
+        <Header scrollY={scrollY} />
+        <AuthContainer
+          icon={<UserCircle size={110} />}
+          title="Sign up to Triangle"
+          subtitle={
+            <>
+              {"Already have an account? "}
+              <Link to={"/"}>Login</Link>
+            </>
+          }
+        >
+          <SignUpForm />
+        </AuthContainer>
+        <Footer />
+      </Container>
+    </>
   );
 }
 
 const Container = styled.div`
-  min-height: 100vh;
+  height: 100%;
   width: 100%;
 
   display: flex;
   flex-direction: column;
   align-items: center;
+
+  overflow-y: auto;
 `;
