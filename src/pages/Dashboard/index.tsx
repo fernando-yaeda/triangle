b9c38464c4c8e5805a69bd7eb9e122e47f0aeac8
fernando yaeda <fernandoyudi.yaeda@gmail.com>
2023-05-19 13:23:51 -0300

    feat(pages): create dashboard base layout

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,3 +1,36 @@
+import styled from "styled-components";
+import { CompletedTasksCard } from "./components/CompletedTasksCard";
+
 export default function Dashboard() {
-  return <div>test</div>;
+  return (
+    <Container>
+      <Row>
+        <CompletedTasksCard />
+      </Row>
+    </Container>
+  );
 }
+
+const Container = styled.div`
+  height: auto;
+  width: 100%;
+
+  display: flex;
+  flex-direction: column;
+  flex-grow: 1;
+`;
+
+const Row = styled.div`
+  height: auto;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-self: flex-start;
+  gap: 20px;
+
+  margin-bottom: 20px;
+
+  @media (max-width: 1300px) {
+    flex-wrap: wrap;
+  }
+`;
