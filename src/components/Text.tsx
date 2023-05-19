65dde29339db4c5827b4337da97fbaf7ca3bd2fc
fernando yaeda <fernandoyudi.yaeda@gmail.com>
2023-05-18 23:46:22 -0300

    refactor(components): add width property to text

--- src/components/Text.tsx
+++ src/components/Text.tsx
@@ -7,12 +7,14 @@ interface TextProps {
   color?: keyof typeof theme.colors;
   variant?: keyof typeof theme.fontVariants;
   type?: "span" | "p" | "h1" | "h2" | "h3" | "h4" | "h5" | "h6";
+  width?: string;
 }
 
 export const Text = styled("span").attrs<TextProps>(({ type = "span" }) => ({
   as: `${type}`,
 }))<TextProps>`
-  ${({ color = "black", variant = "textMd" }) => css`
+  ${({ color = "black", variant = "textMd", width }) => css`
+    width: ${width && width};
     color: ${theme.colors[color]};
     font-size: ${theme.fontVariants[variant].fontSize};
     font-weight: ${theme.fontVariants[variant].fontWeight};
