9810630d9c84762d6e5c549ba2890304d98e91d7
fernando yaeda <fernandoyudi.yaeda@gmail.com>
2023-05-20 13:06:45 -0300

    refactor(components): make page wrapper more responsive

--- src/components/Main/PageWrapper/styles.ts
+++ src/components/Main/PageWrapper/styles.ts
@@ -3,20 +3,18 @@ import styled from "styled-components";
 export const Container = styled.div`
   height: 100%;
   width: 100%;
-
   display: flex;
 `;
 
 export const Left = styled.div`
   min-height: 100vh;
-  width: auto;
   display: flex;
-  flex-shrink: 0;
+  flex: 0 1 auto;
 `;
 
 export const Right = styled.div`
   height: 100vh;
-  width: 100%;
+  flex: 1 1 auto;
 
   display: flex;
   flex-direction: column;
@@ -26,13 +24,12 @@ export const Right = styled.div`
 
 export const ContentWrapper = styled.div`
   height: 100%;
-  width: 100%;
 
   display: flex;
   flex-direction: column;
   justify-content: flex-start;
   align-items: center;
-  flex-grow: 1;
+  flex: 1 1 auto;
 
   padding: 20px 72px;
 
