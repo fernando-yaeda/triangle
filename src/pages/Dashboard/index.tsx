acb38f1a9b76b6fd445d2a33a9e0e6fd845968a8
fernando yaeda <fernandoyudi.yaeda@gmail.com>
2023-05-21 15:48:11 -0300

    feat(pages): add total tasks chart

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,36 +1,28 @@
 import styled from "styled-components";
 import { CompletedTasksCard } from "./components/CompletedTasksCard";
+import { TotalTasksCard } from "./components/TotalTasksCard";
 
 export default function Dashboard() {
   return (
-    <Container>
+    <>
       <Row>
         <CompletedTasksCard />
+        <TotalTasksCard />
       </Row>
-    </Container>
+    </>
   );
 }
 
-const Container = styled.div`
-  height: auto;
-  width: 100%;
-
-  display: flex;
-  flex-direction: column;
-  flex-grow: 1;
-`;
-
 const Row = styled.div`
-  height: auto;
   width: 100%;
   display: flex;
   justify-content: center;
-  align-self: flex-start;
+  align-items: flex-start;
   gap: 20px;
 
   margin-bottom: 20px;
 
-  @media (max-width: 1300px) {
-    flex-wrap: wrap;
+  @media (max-width: 1440px) {
+    height: 340px;
   }
 `;
